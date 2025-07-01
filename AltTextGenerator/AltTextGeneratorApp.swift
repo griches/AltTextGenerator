a960a190a245734a16fe127d762231058348abf3
Gary Riches <gary@bouncingball.mobi>
2025-07-01 08:11:41 +0100

    Initial Commit

---
+++ AltTextGenerator/AltTextGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AltTextGeneratorApp.swift
+//  AltTextGenerator
+//
+//  Created by Gary Riches on 01/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct AltTextGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
